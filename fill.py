e8cb0eb5b69120692237f7a0504b80387252c0ae
muflax <muflax@gmail.com>
2009-09-11 15:57:11 +0200

    dummy file creator

---
+++ fill.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright muflax <mail@muflax.com>, 2009
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+import yaml, random
+from datetime import datetime
+from datetime import timedelta
+
+def main():
+    data = {}
+    entries = [{"sleep": "25m"},
+               {"weight": "78"},
+               {"日本語": "1h20m"}]
+    for i in range(10000):
+        dt = timedelta(days=random.randint(0, 365), seconds=random.randint(0,60*60*24))
+        d = datetime.now() - dt
+        data[d.strftime("%Y-%m-%d %H:%M:%S")] = random.choice(entries).copy()
+
+    print(yaml.dump(data, default_flow_style=False, allow_unicode=True).replace("'", ""))
+
+if __name__ == "__main__":
+    main()
+
