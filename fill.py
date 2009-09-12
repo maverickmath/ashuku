28f64c4414b3048447831fbf81aab879be5c06bc
muflax <muflax@gmail.com>
2009-09-12 02:23:34 +0200

    qualitative bugfix

--- fill.py
+++ fill.py
@@ -11,7 +11,8 @@ def main():
     data = {}
     entries = [{"sleep": "25m"},
                {"weight": "78"},
-               {"日本語": "1h20m"}]
+               {"日本語": "1h20m"},
+               {"happiness": "+2"}]
     for i in range(1000):
         dt = timedelta(days=random.randint(0, 365), seconds=random.randint(0,60*60*24))
         d = datetime.now() - dt
